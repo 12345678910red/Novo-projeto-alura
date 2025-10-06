91c6a488ebf26d56ff50315c3c982a2ab60c8787
12345678910red <eduardo.correia.carlos@escola.pr.gov.br>
2025-10-06 10:25:19 -0300

    Add files via upload

---
+++ Novo-projeto-alura-main/Novo-projeto-alura-main/Novo-projeto-alura-main/assets/perguntas.js
@@ -0,0 +1,19 @@
+criacartao{
+    "progamação",
+    "o que é javali?",
+    "javali e um animal"
+}
+
+
+criacartao{
+    "matematica",
+    " o que é a a formula de baskara",
+    "é uma continha criada por pitagoras"
+}
+
+
+criacartao{
+    "escola",
+    " o que é escola"
+    "é uma instituição de ensino"
+}
